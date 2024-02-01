2235e95a0b854311fe38d028dcabf5e4f56f65cd
FeridQeribov <106318961+Ferid18@users.noreply.github.com>
2024-02-01 12:20:15 +0400

    Update language.py

--- AnonXMusic/utils/decorators/language.py
+++ AnonXMusic/utils/decorators/language.py
@@ -1,8 +1,17 @@
-from strings import get_string
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import get_lang, is_maintenance
+
+from strings import get_string
+from YukkiMusic.misc import SUDOERS
+from YukkiMusic.utils.database import (get_lang, is_commanddelete_on,
+                                       is_maintenance)
 
 
 def language(mystic):
@@ -10,14 +19,13 @@ def language(mystic):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ <a href={SUPPORT_CHAT}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
-                    disable_web_page_preview=True,
+                    "Bot is under maintenance. Please wait for some time..."
                 )
-        try:
-            await message.delete()
-        except:
-            pass
-
+        if await is_commanddelete_on(message.chat.id):
+            try:
+                await message.delete()
+            except:
+                pass
         try:
             language = await get_lang(message.chat.id)
             language = get_string(language)
@@ -33,7 +41,7 @@ def languageCB(mystic):
         if await is_maintenance() is False:
             if CallbackQuery.from_user.id not in SUDOERS:
                 return await CallbackQuery.answer(
-                    f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    "Bot is under maintenance. Please wait for some time...",
                     show_alert=True,
                 )
         try:
